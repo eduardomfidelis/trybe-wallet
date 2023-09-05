3d387987565dcded405fa266ecb9cb9920f4ad5d
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-09-05 16:46:17 -0300

    reuisito(2)Crie um header para a página de carteira com as seguintes características

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+
+  const email = useSelector((state: any) => state.user.email);
+  return (
+    <div>
+      <header>
+        <span data-testid="email-field">{email}</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    </div>
+  );
 }
 
 export default Wallet;
